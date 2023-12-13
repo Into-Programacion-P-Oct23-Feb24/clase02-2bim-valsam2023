ff29dbdec7f57ac217b7cea2786259e35241fd8d
valsam <mvsamaniego1@utpl.edu.ec>
2023-12-13 16:51:57 -0500

    Subir los cambios

---
+++ Ejemplos/src/paquete1/Ejemplo062.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo062 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Declarar y generar arreglos en Java
+        Scanner entrada = new Scanner(System.in);
+        int dato;
+        // arreglo de tipo entero
+        int[] arreglo1 = new int[7];
+        
+        // Se asigna valores al arreglo, pidiendo valores por teclado
+        // se usa un ciclo repetitivo
+        for (int i = 0; i < arreglo1.length; i++) {
+        /*solo se puede ingresar los numeros 5,6,7,8,9  11,13,14 y si pone 
+        cualquier otro numero tiene que salir 5*/
+            System.out.println("Ingrese un valor para el arreglo");
+            dato = entrada.nextInt();
+            if (dato < 5 || dato == 10 || dato == 12 | dato > 14){ 
+            //con or (|) los excluyo
+                dato = 5;
+            }
+            // se asigna la variable dato a la posición del arreglo
+            // de acuerdo al valor de i
+            arreglo1[i] = dato;
+            
+        }
+        
+        // Se hace uso del ciclo repetitivo for para recorrer el arreglo
+        for (int i = 0; i < arreglo1.length; i++) {
+            System.out.printf("Subíndice o Índice %d, con valor %d\n",
+                    i, arreglo1[i]);
+        }
+        
+    }
+
+}
